07df74610646227b07b846582b42c422487f55ee
Alex Baldwin <alexbaldwin2876@gmail.com>
2024-04-17 16:57:56 +0100

    Adding support for controllers :D

--- botd_client.py
+++ botd_client.py
@@ -6,6 +6,7 @@ import json
 from io import BytesIO
 import re
 import sys
+import copy
 import os
 import base64
 import requests
@@ -445,6 +446,195 @@ if __name__ == "__main__":
                     print("Try: pip install keyboard")
                     print("Note: On linux this script requires root")
                     quit()
+            elif (bot_definition["control_scheme"] == "PYGLET_CONTROLLER"):
+                try:
+                    import pyglet
+
+                    from pyglet.shapes import Circle, Rectangle, Arc
+
+
+                    window = pyglet.window.Window(720, 480)
+                    batch = pyglet.graphics.Batch()
+
+
+                    @window.event
+                    def on_draw():
+                        window.clear()
+                        batch.draw()
+
+
+                    class ControllerDisplay:
+                        """A class to visualize all the Controller inputs."""
+
+                        def __init__(self, batch):
+
+                            self.label = pyglet.text.Label("No Controller connected.", x=10, y=window.height - 20,
+                                                        multiline=True, width=720, batch=batch)
+
+                            self.left_trigger = Rectangle(70, 310, 40, 10, batch=batch)
+                            self.right_trigger = Rectangle(610, 310, 40, 10, batch=batch)
+                            self.d_pad = Rectangle(280, 185, 10, 10, batch=batch)
+
+                            self.left_stick = Arc(180, 240, 20, batch=batch)
+                            self.left_stick_label = pyglet.text.Label("(0.00, 0.00)", x=180, y=50, anchor_x='center', batch=batch)
+                            self.left_stick_bar_x = Rectangle(180, 30, 0, 10, batch=batch)
+                            self.left_stick_bar_y = Rectangle(180, 10, 0, 10, batch=batch)
+
+                            self.right_stick = Arc(540, 240, 20, batch=batch)
+                            self.right_stick_label = pyglet.text.Label("(0.00, 0.00)", x=540, y=50, anchor_x='center', batch=batch)
+                            self.right_stick_bar_x = Rectangle(540, 30, 0, 10, batch=batch)
+                            self.right_stick_bar_y = Rectangle(540, 10, 0, 10, batch=batch)
+
+                            self.l_outline1 = Arc(180, 240, 75, color=(44, 44, 44), batch=batch)
+                            self.l_outline2 = Arc(285, 190, 35, color=(44, 44, 44), batch=batch)
+                            self.r_outline1 = Arc(540, 240, 75, color=(44, 44, 44), batch=batch)
+                            self.r_outline2 = Arc(435, 190, 35, color=(44, 44, 44), batch=batch)
+
+                            self.buttons = {'a': Circle(435, 170, 9, color=(124, 178, 232), batch=batch),
+                                            'b': Circle(455, 190, 9, color=(255, 102, 102), batch=batch),
+                                            'x': Circle(415, 190, 9, color=(255, 105, 248), batch=batch),
+                                            'y': Circle(435, 210, 9, color=(64, 226, 160), batch=batch),
+                                            'leftshoulder': Rectangle(70, 290, 40, 10, batch=batch),
+                                            'rightshoulder': Rectangle(610, 290, 40, 10, batch=batch),
+                                            'start': Circle(390, 240, 9, batch=batch),
+                                            'guide': Circle(360, 240, 9, color=(255, 255, 100), batch=batch),
+                                            'back': Circle(330, 240, 9, batch=batch),
+                                            'leftstick': Circle(180, 240, 9, batch=batch),
+                                            'rightstick': Circle(540, 240, 9, batch=batch)}
+                            
+                            self.raw_values = {
+                                    "left_stick_x": 0,
+                                    "left_stick_y": 0,
+                                    "right_stick_x": 0,
+                                    "right_stick_y": 0,
+                                    "left_trigger": 0,
+                                    "right_trigger": 0
+                                }
+
+                            for button in self.buttons.values():
+                                button.visible = False
+
+                        def on_button_press(self, controller, button_name):
+                            if button := self.buttons.get(button_name, None):
+                                button.visible = True
+
+                            controller.rumble_play_weak(1.0, 0.1)
+
+                        def on_button_release(self, controller, button_name):
+                            if button := self.buttons.get(button_name, None):
+                                button.visible = False
+
+                        def on_dpad_motion(self, controller, dpleft, dpright, dpup, dpdown):
+                            position = [280, 185]
+                            if dpup:
+                                position[1] += 25
+                            if dpdown:
+                                position[1] -= 25
+                            if dpleft:
+                                position[0] -= 25
+                            if dpright:
+                                position[0] += 25
+                            self.d_pad.position = position
+
+                        def on_stick_motion(self, controller, stick, xvalue, yvalue):
+                            if stick == "leftstick":
+                                self.raw_values["left_stick_x"] = xvalue
+                                self.raw_values["left_stick_y"] = yvalue
+                                self.left_stick.position = 180+xvalue*50, 240+yvalue*50
+                                self.left_stick_label.text = f"({xvalue:.2f}, {yvalue:.2f})"
+                                self.left_stick_bar_x.width = xvalue * 100
+                                self.left_stick_bar_y.width = yvalue * 100
+                            elif stick == "rightstick":
+                                self.raw_values["right_stick_x"] = xvalue
+                                self.raw_values["right_stick_y"] = yvalue
+                                self.right_stick.position = 540+xvalue*50, 240+yvalue*50
+                                self.right_stick_label.text = f"({xvalue:.2f}, {yvalue:.2f})"
+                                self.right_stick_bar_x.width = xvalue * 100
+                                self.right_stick_bar_y.width = yvalue * 100
+
+                        def on_trigger_motion(self, controller, trigger, value):
+                            if trigger == "lefttrigger":
+                                self.raw_values["left_trigger"] = value
+                                self.left_trigger.y = 310 + (value*50)
+                                #controller.rumble_play_weak(value, duration=5)
+                            elif trigger == "righttrigger":
+                                self.raw_values["right_trigger"] = value
+                                self.right_trigger.y = 310 + (value*50)
+                                #controller.rumble_play_strong(value, duration=5)
+
+
+                    controller_display = ControllerDisplay(batch=batch)
+
+
+                    def on_connect(controller):
+                        controller.open()
+                        controller.rumble_play_weak(1.0, 0.1)
+                        controller_display.label.text = f"Detected: {controller.name}\nController GUID: {controller.guid}"
+                        controller.push_handlers(controller_display)
+
+
+                    def on_disconnect(controller):
+                        controller_display.label.text = "No Controller connected."
+                        controller.remove_handlers(controller_display)
+
+
+                    # ControllerManager instance to handle hot-plugging:
+                    controller_manager = pyglet.input.ControllerManager()
+                    controller_manager.on_connect = on_connect
+                    controller_manager.on_disconnect = on_disconnect
+
+                    # Handle already connected controller:
+                    if controllers := controller_manager.get_controllers():
+                        on_connect(controllers[0])
+                        
+                    def update(dt):
+                        intents = []
+                        expiry = dt + 0.001
+                        
+                        built_intent = {}
+                        if "default_intent" in bot_definition:
+                            built_intent = copy.deepcopy(bot_definition["default_intent"])
+                        #built_intent["expiry"] = expiry
+                        send_intent = False
+                        for keymap in bot_definition["keymap"]:
+                            if keymap["input"] in controller_display.raw_values:
+                                intentval = keymap["intents"]
+                                for i in range(0,len(intentval)):
+                                    intentval[i]["value"] = controller_display.raw_values[keymap["input"]]
+                                intents = intents + intentval
+                                if expiry < keymap["expiry"]:
+                                    expiry = keymap["expiry"]
+                        built_intent["expiry"] = expiry
+                        for intent in intents:
+                            send_intent = True
+                            active = True
+                            if "dead_below" in intent:
+                                if intent["value"] < intent["dead_below"]:
+                                    active = False
+                            if "dead_above" in intent:
+                                if intent["value"] > intent["dead_above"]:
+                                    active = False
+                            if active:
+                                val = (intent["mult"] * intent["value"]) + intent["offset"]
+                                if intent["axis"] in built_intent:
+                                    built_intent[intent["axis"]] = val + built_intent[intent["axis"]]
+                                else:
+                                    built_intent[intent["axis"]] = val
+                        
+                        if "remote_bot_id" in bot_definition:
+                            remote_server_base_uri = "http://" + remote_server
+                            request = requests.post(url = remote_server_base_uri + "/" + bot_definition["remote_bot_id"] + "/cmd", json = built_intent)
+                            data = request.json()
+                        else:
+                            print("NO REMOTE BOT")
+
+                    pyglet.clock.schedule_interval(update, 0.1)
+
+                    pyglet.app.run()
+                except:
+                    print("ERROR!!!")
+                    e = sys.exc_info()
+                    print(e)
             elif (bot_definition["control_scheme"] == "KEYBOARD"):
                 try:
                     import sys
